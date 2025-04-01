f8d69b33de5b540c8fd483532fc7005c2bb9b758
ablorp <adam@cheong.net>
2025-04-01 18:50:45 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@a835a1505c2f82b0923b3846ec329c7136bb99a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "Preprints of papers and posters in reversed chronological order.",
+          description: "Drafts of my papers and essays in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";

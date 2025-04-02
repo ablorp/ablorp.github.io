9e61964532208a9f19743562dba5e1885e6041df
ablorp <adam@cheong.net>
2025-04-02 05:55:58 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@7d9bb0de02fc98b29c3c1e0a6fe24aadb4dfd1bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,9 +40,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-kno3",
+          title: 'KNO3',
+          description: "Homemade Rocket Fuel With Kitchen Chemistry (8th Grade)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",

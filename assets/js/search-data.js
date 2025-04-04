21c8ae6ae36b7a22463b642c8305fa0c5e732daa
ablorp <adam@cheong.net>
2025-04-04 15:51:16 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@64898d11f6756153e7d181939db87388d7a900e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,7 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-why-decussate",
-          title: '🥇why decussate?',
+          title: '🥇why decussate',
           description: "why do vertebrates have decussated cortocospinal tracts?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";

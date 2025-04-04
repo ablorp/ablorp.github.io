47dc69395fde4823fba30837d3f1f17eb724e67e
ablorp <adam@cheong.net>
2025-04-04 16:13:53 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@493b7d93c80dbb74671ef405db97913450c476a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,17 +46,17 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-monte-carlo",
-          title: '🏅monte carlo',
+          title: '🏅 monte carlo',
           description: "Monte Carlo simulation of somatic twist in ancient marine worms",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-why-decussate",
-          title: '🥇why decussate',
+            },},{id: "projects-decussation",
+          title: '🥇 decussation',
           description: "why do vertebrates have decussated cortocospinal tracts?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-cliffhanger-v2",
-          title: '🥇cliffhanger v2',
+          title: '🥇 cliffhanger v2',
           description: "dynamically stable mechanical mountain climbing aid by design",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";

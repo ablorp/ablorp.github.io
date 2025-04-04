714f7de7c6e457d094e9a96ce454a7321850d9eb
ablorp <adam@cheong.net>
2025-04-04 15:53:31 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@daa40f1e1721d04be16aedf93b22dfd419993bd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,13 +60,13 @@ ninja.data = [{
           description: "dynamically stable mechanical mountain climbing aid by design",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-cliffhanger-️",
-          title: 'cliffhanger 🧗‍♂️',
+            },},{id: "projects-️-cliffhanger",
+          title: '🧗‍♂️ cliffhanger',
           description: "dynamically stable mechanical mountain climbing aid by design",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-rocket-fuel",
-          title: 'rocket fuel 🚀',
+          title: '🚀 rocket fuel',
           description: "homemade rocket fuel with kitchen chemistry (i.e., kno3 + sugar etc.)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";

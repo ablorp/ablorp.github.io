a9153d82236daf8bf4a4fd489902e66e789e8a56
ablorp <adam@cheong.net>
2025-04-05 00:48:21 +0000

    Deploying to gh-pages from @ ablorp/ablorp.github.io@f0eb43e4b172c04c02889b6182de2e30a0bd3888 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of cool projects.",
+          description: "A growing collection of cool projects in science and engineering.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
